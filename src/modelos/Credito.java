cb4c3ea250e59493a848a6d42139f3bd51c63c44
unknown <frayosorio@gmail.com>
2026-03-05 13:15:04 -0500

    Se completan los MODELOS del proyecto

--- src/modelos/Credito.java
+++ src/modelos/Credito.java
@@ -1,15 +1,58 @@
-public class Credito extends Cuenta{
+public class Credito extends Cuenta {
 
+    private double valorPrestado;
+    private double tasaInteres;
+    private int plazo;
+    private double saldoRetiro;
 
-    
-    public Credito(String titular, String numero) {
+    public Credito(String titular, String numero, double valorPrestado, double tasaInteres, int plazo) {
         super(titular, numero);
-        
+        this.valorPrestado = valorPrestado;
+        this.tasaInteres = tasaInteres;
+        this.plazo = plazo;
+        this.saldoRetiro = valorPrestado;
+    }
+
+    public double getValorPrestado() {
+        return valorPrestado;
+    }
+
+    public double getTasaInteres() {
+        return tasaInteres;
+    }
+
+    public int getPlazo() {
+        return plazo;
+    }
+
+    public double getSaldoRetiro() {
+        return saldoRetiro;
+    }
+
+    public double getSaldoDeuda() {
+        return valorPrestado - getSaldo();
     }
 
     @Override
     public boolean retirar(double cantidad) {
-        
+        if (saldoRetiro >= cantidad) {
+            saldoRetiro -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    public void pagar(double cantidad) {
+        if (getSaldo() < valorPrestado) {
+            var intereses = getSaldoDeuda() * tasaInteres / 100;
+            var abonoCapital = cantidad - intereses;
+            setSaldo(getSaldo() + abonoCapital);
+        }
+    }
+
+    public double getCuota() {
+        double tasaReal = tasaInteres / 100;
+        return valorPrestado * Math.pow(1 + tasaReal, plazo) * tasaReal / (Math.pow(1 + tasaReal, plazo) - 1);
     }
 
 }
